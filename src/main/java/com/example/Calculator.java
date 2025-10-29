0ac422c512351f4ba126c42fbff95863bcb4db69
Prathamesh1717 <walekarprathamesh8@gmail.com>
2025-10-29 10:15:34 +0530

    Create Calculator.java

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,12 @@
+Calculator.java
+package com.example;
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+Calculator.java
+package com.example;
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
